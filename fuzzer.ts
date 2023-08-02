4710df2e891c9d0fae97f5251f3a162e9077c47f
Campbell Simpson <curse@simpsoncentral.com>
2023-08-01 22:45:14 -0500

    comments and jsdocs, verbosity in config

--- fuzzer.ts
+++ fuzzer.ts
@@ -55,7 +55,7 @@ class Colors {
 export type obj = string|number|boolean|(obj)[]|{[key:string]:obj};
 export type objAnd<T> = T|string|number|boolean|(objAnd<T>)[]|{[key:string]:objAnd<T>};
 export type nullableObj = objAnd<null>;
-export type template = "string"|"integer"|"number"|"boolean"|template[]|{[key:string]:template}|{type:"range",max:number,min:number,isfloat?:boolean}|{type:"enum",values:(string|number)[]};
+export type template = "string"|"integer"|"number"|"boolean"|template[]|{[key:string]:template}|{type:"range",max:number,min:number,isfloat?:boolean}|{type:"enum",values:(string|number|boolean)[]};
 const baseArrayTemplate:template[] = ["string","number","boolean"];
 const baseObjTemplate:{[key:string]:template} = {key1:"string",key2:"number",key3:"boolean"};
 
@@ -68,6 +68,7 @@ import * as fs from "fs"
 
 //#region config
 const config:any = JSON.parse(fs.readFileSync("config.json").toString());
+//get out file config options
 var outFile:string|null = config.outFile;
 if (outFile!=null&&(typeof outFile)!="string") outFile=null;
 if (outFile != null) {
@@ -83,6 +84,10 @@ if (outFile != null) {
         fs.appendFileSync("out.txt",data.join("  ")+"\n");
     };
 }
+//get verbosity config options
+var verbose:boolean = config.verbose;
+if ((typeof verbose) != "boolean") verbose=true;
+//get char set config options
 var charSet:string[];
 if ((typeof config.charSet) != "string") {
     charSet=[];
@@ -91,6 +96,7 @@ if ((typeof config.charSet) != "string") {
     }
 } else charSet=config.charSet.split("")
 
+//get string length config options
 var minStrLen:number = config.minStrLen;
 var maxStrLen:number = config.maxStrLen;
 if ((typeof minStrLen) != "number") { minStrLen=0;        console.log(Colors.Fgra+"invalid config."+Colors.Fr+"minStrLen"+Colors.Fgra+"."+Colors.R); }
@@ -100,15 +106,21 @@ if (maxStrLen<0)                    { minStrLen=0;        console.log(Colors.Fgr
 if (maxStrLen<minStrLen) { maxStrLen=minStrLen;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"maxStrLen"+Colors.Fgra+"."+Colors.R); }
 const StrLenRange:number=maxStrLen-minStrLen;
 
-
+//get integer ocnfig options
 var minInteger:number; if ((typeof config.minInteger) != "number") { minInteger=0;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"minNumber"+Colors.Fgra+"."+Colors.R); } else { minInteger=config.minInteger; }
-var maxInteger:number; if ((typeof config.maxInteger) != "number") { maxInteger=0;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"maxNumber"+Colors.Fgra+"."+Colors.R); } else { maxInteger=config.maxInteger; }
+var maxInteger:number; if ((typeof config.maxInteger) != "number") { maxInteger=minInteger;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"maxNumber"+Colors.Fgra+"."+Colors.R); } else { maxInteger=config.maxInteger; }
 const integerRange:number=maxInteger-minInteger;
+//get float ocnfig options
 var minFloat:number; if ((typeof config.minFloat) != "number") { minFloat=0;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"minFloat"+Colors.Fgra+"."+Colors.R); } else { minFloat=config.minFloat; }
-var maxFloat:number; if ((typeof config.maxFloat) != "number") { maxFloat=0;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"maxFloat"+Colors.Fgra+"."+Colors.R); } else { maxFloat=config.maxFloat; }
+var maxFloat:number; if ((typeof config.maxFloat) != "number") { maxFloat=minFloat;console.log(Colors.Fgra+"invalid config."+Colors.Fr+"maxFloat"+Colors.Fgra+"."+Colors.R); } else { maxFloat=config.maxFloat; }
 const floatRange:number=maxFloat-minFloat;
 //#endregion config
 
+/**
+ * returns random value of the correct type based on the template
+ * @param {template} temp template object
+ * @returns {nullableObj}
+ */
 export function random(temp:template):nullableObj {
     if (temp == "string") return randomString();
     else if (temp == "number") return randomNumber();
@@ -129,26 +141,74 @@ export function random(temp:template):nullableObj {
         }
     } else { return null; }
 }
+/**
+ * returns random float in range defined in the config
+ * @returns {number}
+ */
 function randomNumber():number {
     return Math.round((minFloat+Math.random()*floatRange)*1000)/1000;
 }
+/**
+ * returns random integer in range defined in the config
+ * @returns {number}
+ */
 function randomInteger():number {
     return Math.round((minInteger+Math.random()*integerRange));
 }
+/**
+ * returns random character from a charater set defined in the config
+ * @returns {string}
+ */
 function randomChar():string {
     return charSet[Math.round(Math.random()*(charSet.length-1))]
     //return String.fromCharCode((0x23+Math.ceil(Math.random()*(0x7E-0x23))));
     //return String.fromCharCode(Math.floor(Math.random() * 0xffff));
 }
+/**
+ * returns random string with a random length defined in the config
+ * and from a charater set defined in the config
+ * @returns {string}
+ */
 function randomString():string {
     const len:number = minStrLen+Math.ceil(Math.random()*StrLenRange);
     var str:string = "";
     for (let i:number = 0; i < len; i++) str+=randomChar();
     return str;
 }
+
+/**
+ * returns random true or false value
+ * @returns {boolean}
+ */
 function randomBoolean():boolean {
     return Math.round(Math.random())==1;
 }
+/**
+ * converts a "range" object to a random value in its range
+ * can be an integer range or float
+ * template: {type:"range",max:number,min:number,isfloat?:boolean}
+ * @param {{type:"range",max:number,min:number,isfloat?:boolean}} temp template object
+ * @returns {number}
+ */
+function randomRange(temp:{type:"range",max:number,min:number,isfloat?:boolean}):number {
+    return (temp.isfloat==true)?(temp.min+Math.random()*(temp.max-temp.min)):Math.round(temp.min+Math.random()*(temp.max-temp.min));
+}
+/**
+ * converts a "enum" object to a random value from its set of values
+ * template: {type:"enum",values:(string|number|boolean)[]}
+ * @param {{type:"enum",values:(string|number|boolean)[]}} temp template object
+ * @returns {string|number|boolean}
+ */
+function randomEnum(temp:{type:"enum",values:(string|number|boolean)[]}):string|number|boolean {
+    const vals:(string|number|boolean)[] = temp.values;
+    return vals[Math.round(Math.random()*(vals.length-1))];
+}
+/**
+ * converts each value of the array as a template
+ * to a random value of the correct type
+ * @param {template[]} temp template array
+ * @returns {nullableObj[]}
+ */
 function randomArray(temp:template[]):nullableObj[] {
     var newArry:nullableObj[] = [];
     for (let i:number = 0; i < temp.length; i++) {
@@ -157,14 +217,13 @@ function randomArray(temp:template[]):nullableObj[] {
     }
     return newArry;
 }
-function randomRange(temp:{type:"range",max:number,min:number,isfloat?:boolean}):number {
-    return (temp.isfloat==true)?(temp.min+Math.random()*(temp.max-temp.min)):Math.round(temp.min+Math.random()*(temp.max-temp.min));
-}
-function randomEnum(temp:{type:"enum",values:(string|number|boolean)[]}):string|number|boolean {
-    const vals:(string|number|boolean)[] = temp.values;
-    return vals[Math.round(Math.random()*(vals.length-1))];
-}
-function randomObject(temp:{[key:string]:template}):nullableObj {
+/**
+ * converts each value of the object as a template
+ * to a random value of the correct type
+ * @param {{[key:string]:template}} temp template object
+ * @returns {{[key:string]:nullableObj}}
+ */
+function randomObject(temp:{[key:string]:template}):{[key:string]:nullableObj} {
     var newObj:nullableObj = {};
     const keys = Object.keys(temp);
     for (let i:number = 0; i < keys.length; i++) {
@@ -176,7 +235,13 @@ function randomObject(temp:{[key:string]:template}):nullableObj {
 }
 
 var changes:[(string|number)[],nullableObj][] = []
-function processTemplate(temp:template,indexes:(string|number)[]) {
+/**
+ * finds changes that could potentially break the target based on the json template
+ * and saves them to the "changes" array with indexes and value
+ * @param {template} temp
+ * @param {(string|number)[]} indexes
+ */
+function processTemplate(temp:template,indexes:(string|number)[]):void {
     if (temp == "string") {
         changes.push([[...indexes],null]);
         changes.push([[...indexes],randomInteger()]);
@@ -255,8 +320,16 @@ function processTemplate(temp:template,indexes:(string|number)[]) {
             }
             return;
         }
-    } else { return null; }
+    } else return;
 }
+
+/**
+ * checks if two objects are equal without just converting them to json
+ * end up being slightly faster than convertingto json
+ * @param {nullableObj} a object 1
+ * @param {nullableObj} b object 2
+ * @returns {boolean}
+ */
 function objectEqual(a:nullableObj,b:nullableObj):boolean {
     if ((a===undefined&&b===null)||(a===null&&b===undefined)) return true;// if one is null and the other is undefined the folowing will return false but they are equal;
     const typeofA:string = (typeof a);
@@ -268,20 +341,19 @@ function objectEqual(a:nullableObj,b:nullableObj):boolean {
         var entriesA=Object.entries(a!);
         var entriesB=Object.entries(b!);
         if (entriesA.length!=entriesB.length) return false;
-        for (let i = 0; i < entriesA.length; i++) {
+        for (let i = 0; i < entriesA.length; i++) {//check that each key and value of the object are equal
             if (!objectEqual(entriesA[i][0],entriesB[i][0])) return false;
             if (!objectEqual(entriesA[i][1],entriesB[i][1])) return false;
         }
     }
     return true;
 }
-/*function msToString(ms:number):string {
-    if (ms>1000*60*60*24) return (ms/(1000*60*60*24)).toFixed(3)+" days";
-    else if (ms>1000*60*60) return (ms/(1000*60*60)).toFixed(3)+" hrs";
-    else if (ms>1000*60) return (ms/(1000*60)).toFixed(3)+" mins";
-    else if (ms>1000) return (ms/(1000)).toFixed(3)+" s";
-    else return ms+" ms";
-}*/
+
+/**
+ * generates string for time formatted with color, units, and 3 decimal places
+ * @param {number} ms milliseconds
+ * @returns {string}
+ */
 function msToStringColor(ms:number):string {
     if (ms>1000*60*60*24) return Colors.Fy+(ms/(1000*60*60*24)).toFixed(3).padStart(6,"0")+Colors.Fgra+" days"+Colors.R;
     else if (ms>1000*60*60) return Colors.Fy+(ms/(1000*60*60)).toFixed(3).padStart(6,"0")+Colors.Fgra+" hrs"+Colors.R;
@@ -289,19 +361,26 @@ function msToStringColor(ms:number):string {
     else if (ms>1000) return Colors.Fy+(ms/(1000)).toFixed(3).padStart(6,"0")+Colors.Fgra+" s"+Colors.R;
     else return Colors.Fy+ms.toString().padStart(6,"0")+Colors.Fgra+" ms"+Colors.R;
 }
-/*function ratioToPercentage(a:number,b:number) {
-    return (a/b*100).toFixed(3).padStart(7,"0")+" %";
-}*/
+
+/**
+ * return a/b as a percentage formatted with color, a percentage sign, and 3 decimal places
+ * @param {number} a numerator
+ * @param {number} b denominator
+ * @returns {string}
+ */
 function ratioToPercentageColor(a:number,b:number) {
     return Colors.Fy+(a/b*100).toFixed(3).padStart(7,"0")+Colors.Fgra+" %"+Colors.R;
 }
 
 
-function createTests() {
+/**
+ * create list of "tests" which is a list of every combination of invalid json objects 
+ * 
+ * @returns {nullableObj[]}
+ */
+function createTests():nullableObj[] {
     //creates possible changes
-    var proccessingTime:number = (new Date()).getTime();
     processTemplate(temp,[]);
-    proccessingTime = (new Date()).getTime()-proccessingTime;
 
     var tests:nullableObj[] = [];
     var skips:number = 0;
@@ -309,9 +388,11 @@ function createTests() {
     var generationTime:number = (new Date()).getTime();
     for (let i = 0; i < changes.length; i++) {
         const change1:[(string | number)[], nullableObj] = changes[i];
-
-        console.log(ratioToPercentageColor(i*changes.length, changes.length*(changes.length+1)));
-        console.log(Colors.Fgra+"Time elapsed: "+msToStringColor((new Date()).getTime()-generationTime)+Colors.Fgra+".\n"+Colors.R);
+        //print percentage and time elapsed
+        if (verbose) {
+            console.log(ratioToPercentageColor(i*changes.length, changes.length*(changes.length+1)));
+            console.log(Colors.Fgra+"Time elapsed: "+msToStringColor((new Date()).getTime()-generationTime)+Colors.Fgra+".\n"+Colors.R);
+        }
         for (let j = i+1; j < changes.length; j++) {
             const change2:[(string | number)[], nullableObj] = changes[j];
 
@@ -320,26 +401,30 @@ function createTests() {
             for (let i = 0; i < change2[0].length; i++) {
                 if (change1[0][i]!=change2[0][i]&&change1[0][i]!=null) modifyingParent=false;
             }
-            if (modifyingParent&&!objectEqual(change1,change2)) { skips++; continue; }
+            var areEqual:boolean=objectEqual(change1,change2);
+            if (modifyingParent&&!areEqual) { skips++; continue; }
 
             //change1
             var rand:nullableObj = random(temp);
             if (change1[0].length == 0) rand = change1[1];
             else {
+                //search through indexes of rand object to find the on to be modified
                 var thing:nullableObj = rand;
                 for (let j = 0; j < change1[0].length-1; j++) { thing = (thing! as any)[change1[0][j]]; }
                 (thing! as any)[change1[0][change1[0].length-1]] = change1[1];
             }
 
             //change2
-            try {
-                if (change1[0].length == 0) rand = change2[1];
-                else {
-                    var thing:nullableObj = rand;
-                    for (let j = 0; j < change2[0].length-1; j++) { thing = (thing! as any)[change2[0][j]]; }
-                    (thing! as any)[change2[0][change2[0].length-1]] = change2[1];
-                }
-            } catch (err:any) { continue; }
+            if (!areEqual) {//dont bother with change2 if change1 and 2 are equal
+                try {
+                    if (change1[0].length == 0) rand = change2[1];
+                    else {
+                        var thing:nullableObj = rand;
+                        for (let j = 0; j < change2[0].length-1; j++) { thing = (thing! as any)[change2[0][j]]; }
+                        (thing! as any)[change2[0][change2[0].length-1]] = change2[1];
+                    }
+                } catch (err:any) { continue; }
+            }
 
             //check for duplicate entries
             var isDupe:boolean = false;
@@ -351,23 +436,23 @@ function createTests() {
     }
     generationTime = (new Date()).getTime()-generationTime;
 
-    console.log("\n");
-    console.log(Colors.Fgra+"Processing time: "+msToStringColor(proccessingTime)+Colors.Fgra+"."+Colors.R);
-    console.log(Colors.Fgra+"Generation time: "+msToStringColor(generationTime)+Colors.Fgra+"."+Colors.R);
-    console.log(Colors.Fy+tests.length+Colors.Fgra+" tests found."+Colors.R);
-    console.log(Colors.Fy+skips+Colors.Fgra+" skips."+Colors.R);
-    
+    if (verbose) {
+        console.log("\n");
+        console.log(Colors.Fgra+"Generation time: "+msToStringColor(generationTime)+Colors.Fgra+"."+Colors.R);
+        console.log(Colors.Fy+tests.length+Colors.Fgra+" tests found."+Colors.R);
+        console.log(Colors.Fy+skips+Colors.Fgra+" skips."+Colors.R);
+    }
+
     return tests;
 }
 
-
+//create the tests and runs them in the "fuzz" function
 async function run() {
     const tests:nullableObj[] = createTests();
-    console.log(tests.length);
     for (let i = 0; i < tests.length; i++) {
-        console.log((i+1)+"/"+tests.length);
+        if (verbose) console.log((i+1)+"/"+tests.length);
         await fuzz(tests[i],i==(tests.length-1));
     }
-    console.log("Ran final test.");
+    if (verbose) console.log("Ran final test.");
 }
 run()
